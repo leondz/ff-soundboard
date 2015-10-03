e531b1daf342ffdb48b3bd90a7a94aa546d3f6d8
Leon Derczynski <leonderczynski@gmail.com>
2015-10-03 08:19:24 +0100

    remove spam from sentences file top

--- paper/code/dataset2sents.py
+++ paper/code/dataset2sents.py
@@ -6,12 +6,6 @@ import sys
 
 sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 
-for line in open(sys.argv[1], 'r'):
-	for sent in sent_detector.tokenize(line.strip()):
-		print sent
-
-
-
 # read in corpus
 corpus_filename = sys.argv[1]
 
